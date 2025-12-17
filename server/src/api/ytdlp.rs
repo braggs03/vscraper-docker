bd01379dd5647192277f45e55217734c3479d969
braggs03 <brandon@ragghianti.org>
2025-12-16 23:41:32 -0800

    Added methods to allow functionality to ytdlpclient. Changed functionality of ytdlp client status to allow db serialization and deserialization.

--- server/src/api/ytdlp.rs
+++ server/src/api/ytdlp.rs
@@ -1,104 +1,60 @@
-use axum::extract::{Path, State};
+use axum::extract::State;
 use axum::http::StatusCode;
-use axum::response::IntoResponse;
-use axum::routing::{any, delete, get, post};
+use axum::routing::post;
 use axum::{Json, Router};
 use regex::Regex;
 use serde::{Deserialize, Serialize};
 use sqlx::SqlitePool;
 use std::collections::HashMap;
+use std::fs;
 use std::path::PathBuf;
 use std::process::{ExitStatus, Stdio};
-use std::str::FromStr;
 use std::sync::Arc;
 use tokio::io::{AsyncBufReadExt, BufReader};
 use tokio::process::Command;
 use tokio::sync::mpsc::error::TryRecvError;
 use tokio::sync::mpsc::Sender;
-use tokio::sync::{broadcast, mpsc, Mutex};
+use tokio::sync::{mpsc, Mutex};
 use tracing::{debug, error, info, trace};
 use url::Url;
 
-use crate::DB_URL_DEFAULT;
-
 const YTDLP_DOWNLOAD_UPDATE_REGEX: &str = r"\[download\]\s+(\d+(?:\.\d+)?)%\s+of\s+~?\s+?(\d+(?:\.\d+)?[GMK]iB)\s+at\s+(\d+\.\d+(?:[GMK]i)?B\/s)\s+ETA\s+((\d+:\d+)|(?:Unknown))";
-const YTDLP_LOCATION: &str = "./libs/yt-dlp";
-
-pub fn routes(db: SqlitePool, download_path: PathBuf) -> Router {
-    // let (tx, rx) = broadcast::channel(100);
-    Router::new()
-        .route("/cancel", delete(cancel_download))
-        .route("/check", post(check_url_availability))
-        .route("/start", post(download_from_options))
-        .route("/test", any(get_filename_test))
-        // .route("/ws", get(download))
-        .with_state(YtdlpClient::new(db, download_path))
-}
-
-#[axum::debug_handler]
-async fn check_url_availability(
-    State(ytdlp_client): State<YtdlpClient>,
-    Json(options): Json<DownloadOptions>,
-) -> StatusCode {
-    match ytdlp_client.check_url_availability(&options).await {
-        Ok(status) => {
-            info!(
-                "download status for url: {}, {}",
-                options.url.as_str(),
-                status
-            );
-            match status.success() {
-                true => StatusCode::OK,
-                false => StatusCode::BAD_REQUEST,
-            }
-        }
-        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
-    }
-}
-
-async fn download_from_options(
-    State(ytdlp_client): State<YtdlpClient>,
-    Json(options): Json<DownloadOptions>,
-) -> StatusCode {
-    tokio::spawn(async move {
-        let _ = ytdlp_client.download_from_options(&options).await;
-    });
-
-    StatusCode::CREATED
-}
-
-async fn cancel_download(
-    State(ytdlp_client): State<YtdlpClient>,
-    Json(url): Json<Url>,
-) -> StatusCode {
-    match ytdlp_client.cancel_download(url).await {
-        Ok(status) => match status {
-            Status::Canceled => StatusCode::OK,
-            _ => StatusCode::INTERNAL_SERVER_ERROR,
-        },
-        Err(err) => todo!(),
-    }
-}
-
-// async fn download(
-//     ws: WebSocketUpgrade,
-//     State(state): State<Arc<Mutex<YtdlpClient>>>,
-// ) -> impl IntoResponse {
-//     ws.on_upgrade(move |socket| handle_socket(socket, state))
-// }
+const YTDLP_LOCATION: &str = "yt-dlp";
 
 #[derive(Clone)]
 struct YtdlpClient {
     db: SqlitePool,
     download_path: PathBuf,
-    downloads: Arc<Mutex<HashMap<Url, Status>>>,
+    downloads: Arc<Mutex<HashMap<Url, (Status, Option<Sender<Signal>>)>>>,
 }
 
+#[derive(Debug, Clone, sqlx::Type, Serialize, Deserialize)]
+#[sqlx(type_name = "text", rename_all = "snake_case")] // Store it as a string (TEXT)
 enum Status {
-    None,
     Canceled,
+    Completed,
+    None,
     Paused,
-    Running { tx: Sender<Signal> },
+    Running,
+}
+
+impl std::fmt::Display for Status {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl From<String> for Status {
+    fn from(value: String) -> Self {
+        match value.as_str() {
+            "Canceled" => Status::Canceled,
+            "Completed" => Status::Completed,
+            "None" => Status::None,
+            "Paused" => Status::Paused,
+            "Running" => Status::Running,
+            _ => Status::None,
+        }
+    }
 }
 
 enum Signal {
@@ -108,21 +64,21 @@ enum Signal {
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Debug)]
 pub enum Error {
     NotDownloading,
     FailedToHalt,
 }
+
 #[derive(Clone, Serialize)]
 struct DownloadProgress {
-    url: String,
+    url: Url,
     percent: String,
     size_downloaded: String,
     speed: String,
     eta: String,
 }
 
-#[derive(Clone, Debug, Deserialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct DownloadOptions {
     #[serde(default = "default_container")]
     container: String,
@@ -146,11 +102,11 @@ fn default_quality() -> String {
 }
 
 impl YtdlpClient {
-    pub fn new(db: SqlitePool, download_path: PathBuf) -> YtdlpClient {
+    pub async fn new(db: SqlitePool, download_path: PathBuf) -> YtdlpClient {
         YtdlpClient {
             db: db.clone(),
             download_path,
-            downloads: init_from_db(db),
+            downloads: init_from_db(db).await,
         }
     }
 
@@ -179,16 +135,6 @@ impl YtdlpClient {
         let download_path = self.download_path.clone().join(&options.name_format);
 
         debug!("downloading from url");
-        // let mut child = Command::new(YTDLP_LOCATION)
-        //     .arg("--newline")
-        //     .arg("-f")
-        //     .arg(&options.quality)
-        //     .arg("-o")
-        //     .arg(download_path)
-        //     .arg(options.url.as_str())
-        //     .spawn()
-        //     .unwrap();
-
         let mut child = Command::new(YTDLP_LOCATION)
             .arg("--newline")
             .arg("-f")
@@ -252,9 +198,34 @@ impl YtdlpClient {
                             options.url, pid, err
                         ),
                     }
+
                     match signal {
                         Signal::Cancel => {
-                            let file_name = self.get_filename(options);
+                            let download_file_name = self.get_filename(options).await;
+                            let download_dir_files = std::fs::read_dir(&self.download_path);
+                            if let Some(download_file_name) = download_file_name {
+                                for dir in download_dir_files {
+                                    for file in dir {
+                                        match file {
+                                            Ok(file) => match file.file_name().into_string() {
+                                                Ok(file_name) => {
+                                                    if file_name.contains(&download_file_name) {
+                                                        info!(
+                                                            "removing file: {}",
+                                                            file.file_name()
+                                                                .into_string()
+                                                                .unwrap_or("unknown".to_string())
+                                                        );
+                                                        let _ = fs::remove_file(file.path());
+                                                    }
+                                                }
+                                                Err(_) => todo!(),
+                                            },
+                                            Err(_) => todo!(),
+                                        }
+                                    }
+                                }
+                            }
                         }
                         Signal::Pause => {} // Nothing should done, partially completed files should remain
                     }
@@ -297,15 +268,28 @@ impl YtdlpClient {
         self.downloads
             .lock()
             .await
-            .insert(url, Status::Running { tx });
+            .insert(url, (Status::Running, Some(tx)));
     }
 
     pub async fn cancel_download(&self, url: Url) -> Result<Status> {
         match self.downloads.lock().await.get(&url) {
-            Some(status) => match status {
-                Status::Running { tx } => match tx.send(Signal::Cancel).await {
+            Some(download) => match download {
+                (Status::Running, Some(tx)) => match tx.send(Signal::Cancel).await {
                     Ok(_) => Ok(Status::Canceled),
-                    Err(err) => Err(Error::FailedToHalt),
+                    Err(_) => Err(Error::FailedToHalt),
+                },
+                _ => Err(Error::NotDownloading),
+            },
+            None => Err(Error::NotDownloading),
+        }
+    }
+
+    pub async fn pause_download(&self, url: Url) -> Result<Status> {
+        match self.downloads.lock().await.get(&url) {
+            Some(download) => match download {
+                (Status::Running, Some(tx)) => match tx.send(Signal::Pause).await {
+                    Ok(_) => Ok(Status::Paused),
+                    Err(_) => Err(Error::FailedToHalt),
                 },
                 _ => Err(Error::NotDownloading),
             },
@@ -329,7 +313,9 @@ impl YtdlpClient {
 
     async fn get_filename(&self, options: &DownloadOptions) -> Option<String> {
         let child = Command::new(YTDLP_LOCATION)
-            .arg("--simulate")
+            .arg("-o")
+            .arg("%(title)s")
+            .arg("--get-filename")
             .arg(&options.url.as_str())
             .stderr(Stdio::null())
             .stdout(Stdio::piped())
@@ -346,8 +332,6 @@ impl YtdlpClient {
                             while let Ok(Some(line)) = reader.next_line().await {
                                 last_line = line;
                             }
-                            info!(last_line);
-
                             Some(last_line)
                         }
                         false => None,
@@ -360,26 +344,85 @@ impl YtdlpClient {
     }
 }
 
-fn init_from_db(db: SqlitePool) -> Arc<Mutex<HashMap<Url, Status>>> {
+#[derive(sqlx::Type, Debug, Serialize, Deserialize)]
+struct Download {
+    id: i64,
+    status: Status,
+}
+
+
+async fn init_from_db(db: SqlitePool) -> Arc<Mutex<HashMap<Url, (Status, Option<Sender<Signal>>)>>> {
+    let downloads = sqlx::query_as!(Download, "SELECT * FROM Download")
+            .fetch_all(&db)
+            .await;
+    // Map downloads to map.
+
     Arc::new(Mutex::new(HashMap::new()))
 }
 
-async fn get_filename_test() -> String {
-    let db = SqlitePool::connect(DB_URL_DEFAULT).await.unwrap();
-    let download_path = PathBuf::new();
-    let client = YtdlpClient::new(db, download_path);
+pub async fn routes(db: SqlitePool, download_path: PathBuf) -> Router {
+    // let (tx, rx) = broadcast::channel(100);
+    Router::new()
+        .route("/", post(download_from_options))
+        .route("/cancel", post(cancel_download))
+        .route("/check", post(check_url_availability))
+        .route("/pause", post(pause_download))
+        .with_state(YtdlpClient::new(db, download_path).await)
+}
 
-    let file_name = client
-        .get_filename(&DownloadOptions {
-            url: Url::from_str("https://www.youtube.com/watch?v=5fa2KAUqIzk").unwrap(),
-            container: String::new(),
-            name_format: String::new(),
-            quality: String::new(),
-        })
-        .await;
+async fn download_from_options(
+    State(ytdlp_client): State<YtdlpClient>,
+    Json(options): Json<DownloadOptions>,
+) -> StatusCode {
+    match ytdlp_client.download_from_options(&options).await {
+        Ok(_) => todo!(),
+        Err(_) => todo!(),
+    }
+}
 
-    println!("{:?}", file_name);
+async fn cancel_download(
+    State(ytdlp_client): State<YtdlpClient>,
+    Json(url): Json<Url>,
+) -> StatusCode {
+    match ytdlp_client.cancel_download(url).await {
+        Ok(status) => match status {
+            Status::Canceled => StatusCode::OK,
+            _ => StatusCode::INTERNAL_SERVER_ERROR,
+        },
+        Err(_) => StatusCode::BAD_REQUEST,
+    }
+}
 
-    file_name.unwrap()
+#[axum::debug_handler]
+async fn check_url_availability(
+    State(ytdlp_client): State<YtdlpClient>,
+    Json(options): Json<DownloadOptions>,
+) -> StatusCode {
+    match ytdlp_client.check_url_availability(&options).await {
+        Ok(status) => {
+            info!(
+                "download status for url: {}, {}",
+                options.url.as_str(),
+                status
+            );
+            match status.success() {
+                true => StatusCode::OK,
+                false => StatusCode::BAD_REQUEST,
+            }
+        }
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    }
+}
 
+async fn pause_download(
+    State(ytdlp_client): State<YtdlpClient>,
+    Json(url): Json<Url>,
+) -> StatusCode {
+    match ytdlp_client.pause_download(url).await {
+        Ok(status) => match status {
+            Status::Paused => StatusCode::OK,
+            _ => StatusCode::INTERNAL_SERVER_ERROR,
+        },
+        Err(_) => StatusCode::BAD_REQUEST,
+    }
 }
