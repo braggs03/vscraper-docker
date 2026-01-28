7fcc9a4603415501025830dbc9bcf5e658924511
braggs03 <brandon@ragghianti.org>
2026-01-27 22:54:51 -0800

    Updated packages and remove dead code.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,6 @@ import { Button } from "./components/ui/button";
 import { Checkbox } from "./components/ui/checkbox";
 import { Input } from "./components/ui/input";
 import { Label } from "./components/ui/label";
-import { Progress } from "./components/ui/progress";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
 import { DownloadEntry } from './types';
 
