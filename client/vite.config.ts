f150acf82715dc7e949dd8ee5a4f193e39a4fa20
braggs03 <brandon@ragghianti.org>
2026-02-13 21:45:12 -0800

    Added backend api setting for dev mode in vite config.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,27 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
 
-import path from "path"
 import tailwindcss from "@tailwindcss/vite"
+import path from "path"
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        plugins: [react(), tailwindcss()],
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src"),
+            },
         },
-    },
+        server: {
+            proxy: {
+                '/api': {
+                    target: env.API_URL,
+                    changeOrigin: true,
+                    ws: true,
+                },
+            },
+        },
+    }
 })
